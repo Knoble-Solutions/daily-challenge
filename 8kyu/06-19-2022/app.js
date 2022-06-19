6e884908c24c21588d7a71eb438c4bbc96349905
Knoble-Solutions <106287477+Knoble-Solutions@user.noreply.github.com>
2022-06-19 07:01:44 -0400

    June 19th 2022

--- 8kyu/06-19-2022/app.js
+++ 8kyu/06-19-2022/app.js
@@ -1,1 +1,17 @@
+// Write function bmi that calculates body mass index (bmi = weight / height2).
 
+// if bmi <= 18.5 return "Underweight"
+
+// if bmi <= 25.0 return "Normal"
+
+// if bmi <= 30.0 return "Overweight"
+
+// if bmi > 30 return "Obese"
+
+function bmi(weight, height) {
+    const bmiNum = weight / (height ** 2);
+    if (bmiNum <= 18.5) return 'Underweight';
+    else if (bmiNum <= 25.0) return 'Normal';
+    else if (bmiNum <= 30.0) return 'Overweight';
+    else return 'Obese';
+  }

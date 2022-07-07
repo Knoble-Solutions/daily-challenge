f08ccd60c17421d4ebf0ddf2f2b15a5d59abbfd2
Knoble-Solutions <106287477+Knoble-Solutions@user.noreply.github.com>
2022-07-07 05:29:28 -0400

    July 7th 2022

---
+++ 8kyu/07-07-2022/app.js
@@ -0,0 +1,21 @@
+// If you can't sleep, just count sheep!!
+// Task:
+
+// Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+
+function countSheep(num) {
+    let finalStr = '';
+
+
+    if (num > 0) {
+        for (i = 1; i <= num; i++) {
+          
+           finalStr += `${i} sheep...`
+        };
+    return finalStr
+
+    } else if (num <= 0){
+            return finalStr
+        }
+    }
+
